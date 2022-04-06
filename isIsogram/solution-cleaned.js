9109687b194d62aa58cd6ce50179c45be6785309
Alejandro Rojas <kmachappy@pm.me>
2022-04-06 03:18:35 -0700

    added read me

--- isIsogram/solution-cleaned.js
+++ isIsogram/solution-cleaned.js
@@ -1,28 +1,17 @@
-function isIsogram(str){
-    const lowerCase = str.toLowerCase()
-    const strArray = Array.from(lowerCase)
-    let bool = true
-      strArray.forEach( (element, index, array) =>{
-        const letter1 = element
-        const letter1Index = index
-        
-        console.log(`~letter1: ${letter1} index: ${letter1Index}`)
-      array.forEach((element, index)=>{
-        console.log(element + index)  
-        if(letter1 + letter1Index !== element + index){
-  //         console.log("dupe")
-            if(letter1  === element ){
-              console.log("false")
-              bool = false
-              
-              
-            }
-        } 
-      })
-   
-    })
-    return bool
-  }
-  
-  
-  
+function isIsogram(str) {
+  const lowerCase = str.toLowerCase();
+  const strArray = Array.from(lowerCase);
+  let bool = true;
+  strArray.forEach((element, index, array) => {
+    const letter1 = element;
+    const letter1Index = index;
+    array.forEach((element, index) => {
+      if (letter1 + letter1Index !== element + index) {
+        if (letter1 === element) {
+          bool = false;
+        }
+      }
+    });
+  });
+  return bool;
+}
